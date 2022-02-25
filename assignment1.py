dde125f844bd1537906ab61391e947aa623f16aa
upitroma <55750919+upitroma@users.noreply.github.com>
2022-02-25 17:51:29 +0000

    needs fixing

--- assignment1.py
+++ assignment1.py
@@ -1,10 +1,22 @@
 #!/bin/python3
 #https://www.geeksforgeeks.org/python-program-to-convert-floating-to-binary/
 
+import struct
+def binary(num):
+    return ''.join('{:0>8b}'.format(c) for c in struct.pack('!f', num))
+
+
 print("enter a decimal (ex. 5.5)")
-input = input()
-if input[0] == "-":
-    input = input.lstrip("-")
-    print("number is negative")
-whole, dec = str(input).split(".")
-print(whole, dec)
+input = float(input())
+# if input[0] == "-":
+#     input = input[1:]
+#     print("number is negative")
+# whole, dec = str(input).split(".")
+
+# whole=float(whole)
+# dec=int(dec)
+
+# print(whole, dec)
+# print(binary(whole))
+
+print(binary(input))
